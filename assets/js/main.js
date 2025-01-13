23e69e8156039b91292aaa7e28a1798644113d94
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-13 22:42:34 +0530

    feat: added back to top feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,4 +45,25 @@
       preloader.remove();
     });
   }
+
+  /**
+   * Scroll top button
+   */
+  let scrollTop = document.querySelector(".scroll-top");
+
+  function toggleScrollTop() {
+    if (scrollTop) {
+      window.scrollY > 100 ? scrollTop.classList.add("active") : scrollTop.classList.remove("active");
+    }
+  }
+  scrollTop.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
+  window.addEventListener("load", toggleScrollTop);
+  document.addEventListener("scroll", toggleScrollTop);
 })();
