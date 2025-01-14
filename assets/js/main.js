57935b8974c54b5c0443cba9ed6329f843983bda
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-14 13:24:26 +0530

    feat: initiated `typed.js`

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,4 +66,20 @@
 
   window.addEventListener("load", toggleScrollTop);
   document.addEventListener("scroll", toggleScrollTop);
+
+  /**
+   * Init typed.js
+   */
+  const selectTyped = document.querySelector(".typed");
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
 })();
