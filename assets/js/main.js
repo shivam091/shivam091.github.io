ff8116cde79ec0d8127f2b62e5f8f4524f6c00bb
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-18 12:31:45 +0530

    feat: initialized `aos`

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,4 +82,17 @@
       backDelay: 2000
     });
   }
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener("load", aosInit);
 })();
