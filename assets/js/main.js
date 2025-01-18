a6845fa2ac40e6f21485d089c670839b1d2a7735
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-18 12:34:20 +0530

    feat: initialized scrollspy for navmenu

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,4 +95,26 @@
     });
   }
   window.addEventListener("load", aosInit);
+
+  /**
+   * Navmenu Scrollspy
+   */
+  let navmenulinks = document.querySelectorAll(".navmenu a");
+
+  function navmenuScrollspy() {
+    navmenulinks.forEach(navmenulink => {
+      if (!navmenulink.hash) return;
+      let section = document.querySelector(navmenulink.hash);
+      if (!section) return;
+      let position = window.scrollY + 200;
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        document.querySelectorAll(".navmenu a.active").forEach(link => link.classList.remove("active"));
+        navmenulink.classList.add("active");
+      } else {
+        navmenulink.classList.remove("active");
+      }
+    })
+  }
+  window.addEventListener("load", navmenuScrollspy);
+  document.addEventListener("scroll", navmenuScrollspy);
 })();
