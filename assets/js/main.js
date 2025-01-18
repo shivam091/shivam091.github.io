d65447c051920288dcf6041c8292d26f6966fe3e
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-01-18 14:15:13 +0530

    feat: added content for skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,4 +117,21 @@
   }
   window.addEventListener("load", navmenuScrollspy);
   document.addEventListener("scroll", navmenuScrollspy);
+
+  /**
+   * Animate the skills items on reveal
+   */
+  let skillsAnimation = document.querySelectorAll(".skills-animation");
+  skillsAnimation.forEach((item) => {
+    new Waypoint({
+      element: item,
+      offset: "80%",
+      handler: function(direction) {
+        let progress = item.querySelectorAll(".progress .progress-bar");
+        progress.forEach(el => {
+          el.style.width = el.getAttribute("aria-valuenow") + "%";
+        });
+      }
+    });
+  });
 })();
