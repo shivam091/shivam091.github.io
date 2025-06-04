7b10b3d4a904fdf3e0a6bb5d845a15b82dfc10a8
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-06-04 19:29:56 +0530

    added `progress bar` component  for page loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,9 +86,58 @@ var ScrollTop = (()=> {
   };
 })();
 
+const ProgressBar = (() => {
+  let bar = null;
+  let progress = 0;
+  let interval;
+
+  function showProgress() {
+    if (!bar) return;
+
+    bar.style.width = "0%";
+    bar.setAttribute("data-state", "loading");
+
+    progress = 0;
+
+    clearInterval(interval);
+    interval = setInterval(() => {
+      if (progress < 90) {
+        progress += Math.random() * 10;
+        bar.style.width = `${Math.min(progress, 90)}%`;
+      }
+    }, 200);
+  }
+
+  function completeProgress() {
+    if (!bar) return;
+
+    clearInterval(interval);
+    bar.style.width = "100%";
+
+    setTimeout(() => {
+      bar.setAttribute("data-state", "done");
+      bar.style.width = "0%";
+    }, 500);
+  }
+
+  return {
+    init: () => {
+      bar = document.getElementById("progress-bar");
+
+      if (!bar) return;
+
+      window.addEventListener("beforeunload", showProgress);
+      window.addEventListener("load", completeProgress);
+    },
+    start: showProgress,
+    done: completeProgress
+  };
+})();
+
 (function() {
   "use strict";
 
   PersonalBlog.init();
   ScrollTop.init();
+  ProgressBar.init();
 })();
