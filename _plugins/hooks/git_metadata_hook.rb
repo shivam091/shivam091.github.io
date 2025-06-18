504d8c265fa4b477beba4ad14199a1ab3b171378
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-06-18 20:06:12 +0530

    hook to record `changelog` and `last modified`
    added hook to record `changelog` and `last modified`
    added last modified date in post's `meta`

---
+++ _plugins/hooks/git_metadata_hook.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require "time"
+require "set"
+
+Jekyll::Hooks.register :posts, :pre_render do |post|
+  path = post.path
+  return unless File.exist?(path)
+  return unless system("git", "ls-files", "--error-unmatch", path, out: File::NULL, err: File::NULL)
+
+  # Full changelog with markdown support and ISO strict date+time+tz format
+  log_entries = `git log --reverse --pretty="%H|%ad|%an|%ae|%B---END---" --date=iso-strict "#{path}"`
+
+  seen = Set.new
+  changelog = {}
+  published_at, last_modified_at = nil
+
+  log_entries.split("---END---").each_with_index do |entry, idx|
+    parts = entry.strip.split("|", 5)
+    next unless parts.size == 5
+
+    hash, datetime_raw, author, email, message = parts.map(&:strip)
+    next if seen.include?(hash) || message.empty?
+
+    seen << hash
+
+    begin
+      datetime = Time.parse(datetime_raw).iso8601
+    rescue ArgumentError
+      next
+    end
+
+    published_at ||= datetime
+    last_modified_at = datetime  # Last one will be latest due to `--reverse`
+
+    date_key = datetime[0..9] # "YYYY-MM-DD"
+    type = message[/^(feat|fix|refactor|chore|docs|test|style)(?=\:)/i, 1]&.downcase || "other"
+
+    changelog[date_key] ||= []
+    changelog[date_key] << {
+      "hash" => hash,
+      "datetime" => datetime,
+      "author" => author,
+      "email" => email,
+      "type" => type,
+      "message" => message
+    }
+  end
+
+  post.data["published_at"] = published_at
+  post.data["last_modified_at"] = last_modified_at
+  post.data["changelog"] = changelog unless changelog.empty?
+end
