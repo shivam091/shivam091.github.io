7391c5e936fa244821d1975ffa26efb63a73c5ef
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-06-20 15:57:38 +0530

    improvements in sidebar
    - same duration for animations of toggle button, left margin, and sidebar expand
    - added mask to close sidebar when clicked outside

--- _javascript/modules/components/layouts/sidebar-toggle.js
+++ _javascript/modules/components/layouts/sidebar-toggle.js
@@ -1,12 +1,29 @@
 class SidebarToggle {
-  static TRANSITION_DELAY = 400;
-
   static get sidebarElement() {
     return document.getElementById("sidebar");
   }
 
   static get toggleButtonElement() {
     return document.getElementById("sidebar-toggle");
+  }
+
+  static get maskElement() {
+    return document.getElementById("mask");
+  }
+
+  static get transitionDuration() {
+    const sidebar = this.sidebarElement;
+    if (!sidebar) return 0;
+
+    const durationStr = getComputedStyle(sidebar).getPropertyValue("--sidebar-transition-duration").trim();
+
+    const duration = durationStr.endsWith("ms")
+      ? parseFloat(durationStr)
+      : durationStr.endsWith("s")
+      ? parseFloat(durationStr) * 1000
+      : 0;
+
+    return duration;
   }
 
   static gracefullyClose() {
@@ -19,26 +36,36 @@ class SidebarToggle {
       if (sidebar.getAttribute("data-state") === "close") {
         sidebar.setAttribute("data-state", "idle");
       }
-    }, this.TRANSITION_DELAY);
+    }, this.transitionDuration);
   }
 
-  static initialize() {
+  static toggleSidebar() {
     const sidebar = this.sidebarElement;
     const toggleButton = this.toggleButtonElement;
 
     if (!toggleButton || !sidebar) return;
 
-    toggleButton.addEventListener("click", () => {
-      const isOpen = sidebar.getAttribute("data-state") === "open";
-      const nextState = isOpen ? "close" : "open";
+    const isOpen = sidebar.getAttribute("data-state") === "open";
+    const nextState = isOpen ? "close" : "open";
 
-      sidebar.setAttribute("data-state", nextState);
-      toggleButton.setAttribute("data-state", nextState);
-      toggleButton.setAttribute("aria-expanded", String(nextState === "open"));
-      document.body.setAttribute("data-sidebar", nextState);
+    sidebar.setAttribute("data-state", nextState);
+    toggleButton.setAttribute("data-state", nextState);
+    toggleButton.setAttribute("aria-expanded", String(nextState === "open"));
+    document.body.setAttribute("data-sidebar", nextState);
 
-      if (isOpen) this.gracefullyClose();
-    });
+    if (isOpen) {
+      this.gracefullyClose();
+    }
+  }
+
+  static initialize() {
+    const toggleButton = this.toggleButtonElement;
+    const mask = this.maskElement;
+
+    if (!toggleButton || !mask) return;
+
+    toggleButton.addEventListener("click", this.toggleSidebar.bind(this));
+    mask.addEventListener("click", this.toggleSidebar.bind(this));
   }
 }
 
