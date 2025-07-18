7a5a5a0cd956b6cf922204a58eeb57c933f8bd6d
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-07-18 20:30:11 +0530

    feat: added `pinned` posts generator & layout

---
+++ _plugins/generators/pinned_posts_generator.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative "slug_based_posts_generator"
+require_relative "./../helpers/logger_helper"
+
+module Jekyll
+  class PinnedPostsGenerator < Generator
+    include SlugBasedPostsGenerator, LoggerHelper
+
+    safe true
+    priority :low
+
+    def generate(site)
+      info("▶ PinnedPostsGenerator:", "Generating pinned posts")
+
+      generate_posts(site, "pinned", "pinned_posts")
+    end
+  end
+end
