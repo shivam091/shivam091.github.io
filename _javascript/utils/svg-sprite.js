b9b9d59c6d527acf6ecc81660047f5092b871aab
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-08-03 22:41:13 +0530

    ui: misc js improvements
    - fixed bug causing `tooltip-arrow` to hide on update
    - refactored `generateId` and `createElement` functions
    - renamed `toggle` to `setUseHref` in `SvgSprite` utils
    - allow `data-copy-url` in `Clipboard`
    - added `duration` for color swaps

--- _javascript/utils/svg-sprite.js
+++ _javascript/utils/svg-sprite.js
@@ -1,3 +1,3 @@
-export function toggle(element, symbolHref) {
+export function setUseHref(element, symbolHref) {
   if (element) element.setAttribute("href", `/assets/img/sprite.svg${symbolHref}`);
 }
