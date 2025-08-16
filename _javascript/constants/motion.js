172c3b814640f73f0ffe36895c4b72bc56f145cc
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-08-16 12:17:21 +0530

    ui: added animation for `details` marker

--- _javascript/constants/motion.js
+++ _javascript/constants/motion.js
@@ -1,3 +1,11 @@
+export const MOTION_DURATIONS = {
+  medium: 300
+}
+
+export const MOTION_EASING_CURVES = {
+  entrance: "cubic-bezier(0.16, 1, 0.3, 1)",
+};
+
 export const COLOR_SWAP_TRANSITION = {
   duration: 300,
   easing: "cubic-bezier(0.41, 0.1, 0.13, 1)",
