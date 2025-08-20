e46508dfa0bf3ce38913f3c9ec598f8a52d73002
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-08-20 16:55:16 +0530

    Added animation for `clipboard` icon

--- _javascript/utils/clipboard.js
+++ _javascript/utils/clipboard.js
@@ -1,4 +1,4 @@
-import * as SvgSprite from "./svg-sprite";
+import ClipboardCheck from "./../animations/clipboard-check";
 import Tooltip from "./../modules/components/tooltip";
 
 export default class Clipboard {
@@ -11,6 +11,9 @@ export default class Clipboard {
 
     this.button = this.findButton();
     this.textSource = this.findTextSource();
+
+    this.checkSvg = this.button?.querySelector(".icon-clipboard-check");
+    this.checkAnimator = this.checkSvg ? new ClipboardCheck(this.checkSvg) : null;
 
     this.attachEvents();
   }
@@ -68,6 +71,11 @@ export default class Clipboard {
     const iconEl = triggerer.querySelector("use");
     const originalIcon = iconEl?.getAttribute("href").split("#")[1];
     const originalLabel = triggerer.getAttribute("data-tooltip") || "";
+
+    if (this.checkAnimator) {
+      this.checkAnimator.showCheck();
+      setTimeout(() => this.checkAnimator.resetCheck(), 1500);
+    }
 
     if (iconEl) {
       SvgSprite.setUseHref(iconEl, "#icon-check");
