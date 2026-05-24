9c63debd4354beba1397902f9a4b66848b747b05
Harshal LADHE <harshal.ladhe.1@gmail.com>
2026-05-24 12:58:11 +0530

    feat: added `SkyBannerBottom` component

--- src/app/(internal)/layout.tsx
+++ src/app/(internal)/layout.tsx
@@ -1,6 +1,7 @@
 import { JSX } from "react";
 import PageHeading from "@/components/PageHeading/PageHeading";
 import SkyBannerTop from "@/components/SkyBanners/SkyBannerTop/SkyBannerTop";
+import SkyBannerBottom from "@/components/SkyBanners/SkyBannerBottom/SkyBannerBottom";
 
 export default function InternalLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
@@ -23,6 +24,8 @@ export default function InternalLayout({ children }: { children: React.ReactNode
           </main>
         </div>
       </div>
+
+      <SkyBannerBottom />
     </>
   );
 }
