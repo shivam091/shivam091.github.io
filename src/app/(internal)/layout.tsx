07e8aed1224aa4f56306bce2131d13de456f5b31
Harshal LADHE <harshal.ladhe.1@gmail.com>
2026-05-24 12:53:09 +0530

    Added `SkyBannerTop` component

--- src/app/(internal)/layout.tsx
+++ src/app/(internal)/layout.tsx
@@ -1,8 +1,12 @@
+import { JSX } from "react";
 import PageHeading from "@/components/PageHeading/PageHeading";
+import SkyBannerTop from "@/components/SkyBanners/SkyBannerTop/SkyBannerTop";
 
 export default function InternalLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <>
+      <SkyBannerTop />
+
       <div className="container w-full mx-auto">
         <div className="relative flex flex-col min-h-dvh z-10">
           <main id="main-content" className="flex-1 p-4 max-w-full" style={{ containerType: "inline-size" }} tabIndex={-1}>
